d809a9e5b6671157d90cef3ba799eea97e88c37a
Efemiaya Favour Tega <efemiayafavour@gmail.com>
2025-02-02 01:42:49 +0100

    correction 2

--- app/(main)/transaction/create/page.jsx
+++ app/(main)/transaction/create/page.jsx
@@ -1,31 +1,29 @@
-"use server";
-
-import dynamic from 'next/dynamic';
 import { getUserAccounts } from "@/actions/dashboard";
 import { defaultCategories } from "@/data/categories";
-import React from "react";
 import AddTransactionForm from "./_components/AddTransactionForm";
 import { getTransaction } from "@/actions/transaction";
 
 const AddTransactionPage = async ({ searchParams }) => {
   const accounts = await getUserAccounts();
+  const editId = searchParams?.edit;
 
-  const { edit: editId } = searchParams;
   let initialData = null;
   if (editId) {
-    const transacton = await getTransaction(editId);
-    initialData = transacton;
+    const transaction = await getTransaction(editId);
+    initialData = transaction;
   }
 
   return (
     <div className="max-w-3xl mx-auto px-5">
-      <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold gradient-title mb-5">
-       {!!editId ? "Edit Transaction" : "Add Transaction"}
-      </h1>
-
-      <AddTransactionForm accounts={accounts} categories={defaultCategories} 
-      editMode={!!editId}
-      initialData={initialData} />
+      <div className="flex justify-center md:justify-normal mb-8">
+        <h1 className="text-5xl gradient-title ">Add Transaction</h1>
+      </div>
+      <AddTransactionForm
+        accounts={accounts}
+        categories={defaultCategories}
+        editMode={!!editId}
+        initialData={initialData}
+      />
     </div>
   );
 };
