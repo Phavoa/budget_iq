d951852e3a9fd70eee9276a5e15c2b3f8e8a2b8e
Efemiaya Favour Tega <efemiayafavour@gmail.com>
2025-02-02 07:59:04 +0100

    chore: added dark mode

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "sonner";
+import Header from "@/components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,17 +17,18 @@ export default function RootLayout({ children }) {
       <html lang="en" suppressHydrationWarning>
         <head>{/* <link rel="icon" href="/logo-sm.png" sizes="any" /> */}</head>
         <body className={`${inter.className}`}>
-            <main className="min-h-screen">{children}</main>
-            <Toaster richColors />
+          <Header />
+          <main className="min-h-screen">{children}</main>
+          <Toaster richColors />
 
-            <footer className="bg-blue-50 py-12">
-              <div className="container mx-auto px-4 text-center text-gray-600">
-                <p>
-                  Made with ❤️ by{" "}
-                  <a href="https://github.com/Phavoa">Efemiaya Tega</a>
-                </p>
-              </div>
-            </footer>
+          <footer className="bg-blue-50 py-12">
+            <div className="container mx-auto px-4 text-center text-gray-600">
+              <p>
+                Made with ❤️ by{" "}
+                <a href="https://github.com/Phavoa">Efemiaya Tega</a>
+              </p>
+            </div>
+          </footer>
         </body>
       </html>
     </ClerkProvider>
